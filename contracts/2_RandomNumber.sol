1899f6342233fe768eafd58d865070f2135a2470
alquemiste55555 <shane.mark.dowling@gmail.com>
2022-12-08 19:28:09 +0000

    Update to use VRF V2

--- contracts/2_RandomNumber.sol
+++ contracts/2_RandomNumber.sol
@@ -2,30 +2,41 @@
 
 pragma solidity ^0.8.11;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract RandomNumber is VRFConsumerBase {
+contract RandomNumber is VRFConsumerBaseV2 {
 
-    bytes32 internal keyHash; // identifies which Chainlink oracle to use
-    uint internal fee;        // fee to get random number
-    uint public randomResult;
+    uint256 private constant ROLL_IN_PROGRESS = 42;
 
-    constructor()
-        VRFConsumerBase(
-            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF coordinator
-            0x326C977E6efc84E512bB9C30f76E30c160eD06FB  // LINK token address on Goerli
-        ) {
-            keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
-            fee = 0.25 * 10 ** 18;    // 0.25 LINK
-        }
+    VRFCoordinatorV2Interface COORDINATOR;
 
-    function getRandomNumber() public returns (bytes32 requestId) {
-        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK in contract");
-        return requestRandomness(keyHash, fee);
+    uint64 s_subscriptionId = 7420;
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+    bytes32 s_keyHash = 0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 40000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+    address owner;
+    uint256 public randomResult;
+
+    constructor() VRFConsumerBaseV2(vrfCoordinator) {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        owner = msg.sender;
+    }
+   
+    function getRandomNumber() public returns (uint256 requestId) {
+        requestId = COORDINATOR.requestRandomWords(
+            s_keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
     }
 
-    function fulfillRandomness(bytes32 requestId, uint randomness) internal override {
-        randomResult = randomness;
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        randomResult = (randomWords[0] % 10) + 1;
     }
-
+    
 }
